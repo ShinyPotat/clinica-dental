8dafa4b89ef554a1f84ce231de4c562e20ee96f5
j23rl07 <j23rl07@gmail.com>
2020-05-29 20:37:34 +0200

    mas reutilizacion de codigo

---
+++ src/php/about-us.php
@@ -0,0 +1,38 @@
+<?php
+session_start();
+
+	if(!isset($_SESSION["login"])){
+		header("login.php");
+	}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8" lang="es">
+    <title>Inicio</title>
+    <link rel="stylesheet" type="text/css" href="../css/about-us.css">
+</head>
+
+<body>
+    
+    <?php include_once ("cabeceraR.php"); ?>
+
+    <img class="mascota" src="../../images/mascota.png" alt="mascota.png" width=35% height=45%>
+
+    <div class="bloqueIzquierda">
+        <pre class="texto">Somos el grupo IS-G3-JMCV-Clínica dental 10, formado por: 
+        José Luis Alonso Rocha        joseluisalro20@gmail.com
+        Javier Pacheco Marquez        pachecomarquezjavier@gmail.com
+        Jaime Ramos Lara              j23rl07@gmail.com
+        Alberto Antonio Tokos Matas   belatm12@gmail.com</pre>
+        <a href="#" class="link1">Link a red social</a>
+        <a href="#" class="link2">Link a red social</a>
+        <a href="#" class="link3">Link a red social</a>
+
+        <img class="twitter" src="../../images/twitter.png" width=2% height=4%>
+        <img class="facebook" src="../../images/facebook.png" width=2% height=4%>
+        <img class="instagram" src="../../images/instagram.png" width=2% height=4%>
+    </div>
+    <script src="../js/hora.js"></script>
+</body>
---
+++ src/php/cabeceraR.php
@@ -0,0 +1,19 @@
+<a href="log.html" ><img class="imagen" src="../../images/logo.png" alt="logo.png" width=23% height=23%></a>
+
+    <div class="block">
+        <a href="about-us.html" class="acerca">Acerca de nosotros</a>
+        <!-- Estos bloques definen las id que se usan para el js de la hora -->
+        <div id="box">
+            <div id="box-date"></div>
+            <div id="box-time"></div>
+        </div>
+        
+        <img class="calendario" src="../../images/calendario.png" width="1%" height="11%">
+        <img class="reloj" src="../../images/reloj.png" width="1%" height="11%">
+        
+        <img class="usuario" src="../../images/user.png" width="1.5%" height="13%">
+        
+        <img class="flechaA" src="../../images/flechaA.png" width="20" height="20">
+        
+        <a href=logaut.php class="botonUsuario">Desconectarse</a>
+    </div>
